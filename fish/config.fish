0da7f41d760ba1e1ba9ca05ec43d991cd824c9d1
Suhodolets Ilya <iliasuhodolets@gmail.com>
2025-07-20 23:45:04 +0300

    fish & mako configs

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fastfetch
+
+end
+
+#fnm
+export PATH="/run/user/1000/fnm_multishells/21635_1751790537814/bin":"$PATH"
+export FNM_MULTISHELL_PATH="/run/user/1000/fnm_multishells/21635_1751790537814"
+export FNM_VERSION_FILE_STRATEGY="local"
+export FNM_DIR="/home/ilyaaa/.local/share/fnm"
+export FNM_LOGLEVEL="info"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+export FNM_COREPACK_ENABLED="false"
+export FNM_RESOLVE_ENGINES="true"
+export FNM_ARCH="x64"
