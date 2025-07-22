68add1a263e11f73784736ece81aabe68bb9384e
Suhodolets Ilya <iliasuhodolets@gmail.com>
2025-07-22 20:36:27 +0300

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,13 @@ if status is-interactive
 
 end
 
-#fnm
-export PATH="/run/user/1000/fnm_multishells/21635_1751790537814/bin":"$PATH"
-export FNM_MULTISHELL_PATH="/run/user/1000/fnm_multishells/21635_1751790537814"
-export FNM_VERSION_FILE_STRATEGY="local"
-export FNM_DIR="/home/ilyaaa/.local/share/fnm"
-export FNM_LOGLEVEL="info"
-export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
-export FNM_COREPACK_ENABLED="false"
-export FNM_RESOLVE_ENGINES="true"
-export FNM_ARCH="x64"
+function yz
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+fnm env | source
